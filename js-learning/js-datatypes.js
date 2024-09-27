92a6c786e2b8329e19f7548e58185b92f10e29d4
SagarAli756 <sagar_aliq14@yahoo.com>
2024-09-27 21:52:45 +0000

    datatypes and datatype conversion

---
+++ js-learning/js-datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all JS code as newer version
+// alert(4+4);  // this way of calling alert is used in browser from console, not in nodejs
+
+let name = "Sagar";  // string datatype
+let age = 30;  // number datatype
+let isLoggedIn = true;  // boolean datatype
+
+// data types
+// number => 2 to power of 53
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined
+// symbole => used for uniqueness
+// object
+
+console.log(typeof 78);  // typeof is used to find the data type
+
+console.log(typeof null);  // output will be object because it is a object in javascript
+console.log(typeof undefined);  // output will be undefined because it is a type in javascript
