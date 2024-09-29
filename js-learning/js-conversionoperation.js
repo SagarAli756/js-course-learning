a5de9bb91511471a61b29056f31e5c75825a2406
SagarAli756 <sagar_aliq14@yahoo.com>
2024-09-29 14:44:12 +0000

    operations

--- js-learning/js-conversionoperation.js
+++ js-learning/js-conversionoperation.js
@@ -1,3 +1,5 @@
+//  Conversions  //
+
 let score = "89abc";
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -8,4 +10,34 @@ let valueInNumber = Number(score);
 
 let isLoggedIn = "ljkl";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 765;
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  Operations  //
+
+let value = 87;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(60+60);
+// console.log(70-60);
+// console.log(5*5);
+// console.log(3**3);  // double multiplication means 3 power 3
+// console.log(60/30);
+// console.log(12%8);  // remainder
+
+let str1 = "Welcome,";
+let str2 = " Sagar Ali";
+let str3 = str1 + str2;
+// console.log(str3);
+
+let gameCounter = 254;
+++gameCounter;
+console.log(gameCounter);
+
+
